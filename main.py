1cd953e9f2510a9fd93568181b0946a8d7ce5e17
lmraine <55120024+lmraine@users.noreply.github.com>
2024-01-10 12:12:55 -0600

    added solution

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 #Your code goes here
 def palindrome(word):
     pass
+
+#Solution
+'''def palindrome(word):
+    #result = True
+    last_index = -1
+    
+    for char in word:
+        if char == word[last_index]:
+            last_index -= 1
+        else:
+            return False
+
+    return True  
+
+if __name__ == '__main__':
+    word = input("Enter a word:")
+
+    word = word.lower()
+
+    word = word.strip()
+
+    word = word.replace(" ","")
+
+    result = palindrome(word)
+
+    if result:
+        print("Palindrome")
+    else:
+        print("Not a Palindrome")'''
