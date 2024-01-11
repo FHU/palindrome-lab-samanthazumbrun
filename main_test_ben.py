19a11c95d0b7c93ed5ae5aebc65a1e578e9cd0c7
lmraine <55120024+lmraine@users.noreply.github.com>
2024-01-11 10:04:24 -0600

    name test cases

---
+++ main_test_ben.py
@@ -0,0 +1,5 @@
+import main
+
+def test_main():
+    word = "Benjamin"
+    assert main.palindrome(word)== False
---
+++ main_test_hannah.py
@@ -0,0 +1,5 @@
+import main
+
+def test_main():
+    word = "Hannah"
+    assert main.palindrome(word)== True
