2a95f4cb3a8d08270a8a7cb83275fc9f4d644409
lmraine <55120024+lmraine@users.noreply.github.com>
2024-01-15 17:05:01 -0600

    remove soln

--- main.py
+++ main.py
@@ -1,32 +1,5 @@
-#Your code goes here
+#REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
     pass
 
-#Solution
-'''#Your code goes here
-def palindrome(word):
-    #result = True
-    last_index = -1
-    
-    if len(word) == 0:
-        return False
-    else:
-        for char in word:
-            if char == word[last_index]:
-                last_index -= 1
-            else:
-                return False
-
-        return True  
-
-word = input("Enter a word:")
-
-word = word.lower()
-
-word = word.strip()
-
-word = word.replace(" ","")
-
-result = palindrome(word)
-
-print(result)'''
+#YOUR CODE GOES HERE
